7d9541a189085d7f03cc63be4821b49f0abc8dac
_.mindang <minhdang875425@gmail.com>
2025-06-29 09:53:37 +0700

    push

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,19 +1,40 @@
 import CompanionsForm from "@/components/CompanionForm"
+import { newCompanionPermissions } from "@/lib/actions/companion.actions"
 import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import Image from "next/image"
+import Link from "next/link"
 
 const NewCompanion = async () => {
     const { userId } = await auth()
     if (!userId)
         redirect('/sign-in')
 
+    const canCreateCompanion = await newCompanionPermissions()
+
     return (
         <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-            <article className="w-full gap-4 flex flex-col">
+            { canCreateCompanion  ? (<article className="w-full gap-4 flex flex-col">
                 <h1>Companion Builder</h1>
 
                 <CompanionsForm />
             </article>
+            ) : (
+                <article className="companion-limit">
+                    <Image 
+                        src="/images/limit.svg"
+                        alt="Companion limit reached"
+                        width={350}
+                        height={230}
+                    />
+                    <div className="cta-badge">Upgrade Your Plan</div>
+                    <h1>You've Reached Your Limit</h1>
+                    <p>You’ve reached your companion limit. Upgrade to create more companions and premium features.</p>
+                    <Link href="/subscription" className="btn-primary">
+                        Upgrade My Plan
+                    </Link>
+                </article>
+            )}
         </main>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,43 +3,31 @@ import CompanionsList from '@/components/CompanionList'
 import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSession } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 })
+  const recentSessionsCompanions = await getRecentSession(10)
+
   return (
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
 
       <section className="home-section">
-        <CompanionsCard 
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="Science"
-          duration={45}
-          color="#E5D0FF"
-        />
-        <CompanionsCard 
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="Maths"
-          duration={30}
-          color="#FFDA6E"
-        />
-        <CompanionsCard 
-          id="789"
-          name="Verba the Vocabulary Builder"
-          topic="English Literature"
-          subject="Language"
-          duration={30}
-          color="#BDE7FF"
-        />
+        {companions.map(companion => (
+          <CompanionsCard 
+            key={companion.id}
+            { ... companion }
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
 
       <section className="home-section">
         <CompanionsList 
           title="Recently completed sessions"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
